4f5630e046f44d324353c428091c20319148866b
BP--- <bp_114@hotmail.com>
2025-08-10 19:18:08 +0200

    added temp test file

---
+++ food_label.py
@@ -0,0 +1,40 @@
+from escpos.printer import Usb
+
+# --- Configuration ---
+PRINTER_VENDOR_ID = 0x0fe6
+PRINTER_PRODUCT_ID = 0x811e
+
+def test_print():
+    """Initializes the printer with a specific output endpoint."""
+    try:
+        # THE KEY CHANGE IS HERE: we add out_ep=0x02
+        p = Usb(PRINTER_VENDOR_ID, PRINTER_PRODUCT_ID, 0, profile="simple", in_ep=0x82,  out_ep=0x03)
+        print(p.is_usable())
+        p.open()
+        p.set(align='center', bold=True)
+        p.text("BPs jerky\n")
+        p.textln("----------")
+        
+        p.set(align='left', bold=False)
+        p.ln(2)
+        p.textln("Oksekjøtt")
+        p.textln("Worcestershire saus")
+        p.textln("Soya saus")
+        p.textln("Brunt sukker")
+        p.textln("Løkpulver")
+        p.textln("Hvitløkspulver")
+        p.textln("Paprikapulver")
+        p.textln("Gochugaru")
+        p.textln("Kajennepepper")
+        p.ln(2)
+        #p.cut()
+        
+        print("Test message sent to printer successfully!")
+        p.close()
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+# --- Run the test print ---
+if __name__ == "__main__":
+    test_print()
